a55326542e7921d48e00a906bac14adf03ed851e
Adel <adel-s@outlook.com>
2017-07-07 23:11:54 -0600

    URL Shortener app built using Django

---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/Adel/Desktop/Python/Coding_for_Entrepreneurs/trydjango110/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
